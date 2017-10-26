8e98437819b0215858e5c7dbdbd983a85148e594
mistereal <mistereal@gmail.com>
2017-10-26 09:52:42 +0200

    example regarding nunit
    example regarding nunit

---
+++ CircuitCalculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitCalculatorN
+{
+
+    /*
+ I= V/R   V=6
+          R=3    
+          I=2
+     */
+
+    public class CircuitCalculator
+    {
+        const int v = 6;
+        const int i = 2;
+        const int r = 3;
+
+        public decimal GetResistence()
+        {
+            return v/i;
+
+        }
+
+        public decimal GetIntensity()
+        {
+            return v/r;
+
+        }
+
+        public decimal GetVoltage()
+        {
+            return r * i;
+
+        }
+
+    }
+}
